cd7e40852c10580acfc4c5235ea63679d1afaa74
lucasafonsobastos <lucasafonsob@hotmail.com>
2024-05-18 18:16:36 -0300

    atualizando projeto

--- tdd_fastapi/store/main.py
+++ tdd_fastapi/store/main.py
@@ -5,13 +5,12 @@ from store.routers import api_router
 
 
 class App(FastAPI):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(
             *args,
             **kwargs,
             version="0.1.0",
-            title=settings.PROJECT_NAME,
-            root_path=settings.ROOT_PATH
+            title="Store API",
         )
 
 
